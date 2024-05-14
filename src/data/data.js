47be8f178ff5f9c4441dcb852025a22c1d91fa58
souhailelghi <souhailelghioune98@gmail.com>
2024-05-14 11:55:25 -0700

    Add data for testing the code

---
+++ src/data/data.js
@@ -0,0 +1,224 @@
+export const demo = [
+    {
+        id : 1,
+        title : "Why cats are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
+        img : '../1.jpg',
+        questions : [
+            {
+                id : 1,
+                qs : 'do you love cats',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'yes',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'no',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'maybe',
+                        count : 0
+                    }
+                ]
+            },
+            {
+                id : 2,
+                qs : 'how many do you cat have',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '4',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '3',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '2',
+                        count : 0
+                    }
+                ]
+            },
+            {
+                id : 3,
+                qs : "what your favorite cat's color ?",
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'white',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'brown',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'other',
+                        count : 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id : 2,
+        title : "Why dogs are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
+        img : '../2.jpg',
+        questions : [
+            {
+                id : 1,
+                qs : 'do you love dogs',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'yes',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'no',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1) ,
+                        op : 'maybe',
+                        count : 0
+                    }
+                ]
+            },
+            {
+                id : 2,
+                qs : 'how many do you dog have',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '4',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '3',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '2',
+                        count : 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id : 3,
+        title : "Why birds are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
+        img : '../3.jpg',
+        questions : [
+            {
+                id : 1,
+                qs : 'do you love birds',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'yes',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'no',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'maybe',
+                        count : 0
+                    }
+                ]
+            },
+            {
+                id : 2,
+                qs : 'how many do you bird have',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '4',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '3',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '2',
+                        count : 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id : 4,
+        title : "Why snakes are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
+        img : '../4.jpg',
+        questions : [
+            {
+                id : 1,
+                qs : 'do you love snake',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'yes',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'no',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : 'maybe',
+                        count : 0
+                    }
+                ]
+            },
+            {
+                id : 2,
+                qs : 'how many do you snake have',
+                options : [
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '4',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '3',
+                        count : 0
+                    },
+                    {
+                        id : Date.now()*(Math.floor(Math.random()*999)+1),
+                        op : '2',
+                        count : 0
+                    }
+                ]
+            }
+        ]
+    }
+] 
+
